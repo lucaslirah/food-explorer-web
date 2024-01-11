d715f4d5e264d25d847b6211e23ef48830447b81
lucaslira <lucasliraone@gmail.com>
2024-01-11 16:59:14 -0300

    Redirecionando rotas e Login

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 
 import { api } from '../services/api';
 
@@ -12,6 +12,9 @@ function AuthProvider({ children }){
             const response = await api.post("/sessions", {email, password});
             const { user, token } = response.data;
 
+            localStorage.setItem("@foodexplorer:user", JSON.stringify(user));
+            localStorage.setItem("@foodexplorer:token", token);
+
             api.defaults.headers.authorization = `Bearer ${token}`;
             setData({ user, token });
 
@@ -24,8 +27,24 @@ function AuthProvider({ children }){
         }
     }
 
+    useEffect(() => {
+        const token = localStorage.getItem("@foodexplorer:token");
+        const user = localStorage.getItem("@foodexplorer:user");
+
+        if(token && user){
+            api.defaults.headers.authorization = `Bearer ${token}`;
+
+            setData({
+                token,
+                user: JSON.parse(user)
+            });
+        }
+    }, []);
+
     return(
-        <AuthContext.Provider value={{ signIn, user:data.user }}>
+        <AuthContext.Provider value={{ 
+            signIn,
+            user:data.user }}>
             {children}
         </AuthContext.Provider>
     )
